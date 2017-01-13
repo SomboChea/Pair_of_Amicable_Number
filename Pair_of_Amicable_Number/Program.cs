72d075962610e2d2223d9bc152eefee271584351
Sambo Chea <sombochea100@gmail.com>
2017-01-13 16:06:25 +0700

    Add project files.

---
+++ Pair_of_Amicable_Number/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pair_of_Amicable_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
