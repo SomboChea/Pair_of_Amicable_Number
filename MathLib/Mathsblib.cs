b5c9cac8c11f6a5057a16d7ddf3fe879872323b8
Sambo Chea <sombochea100@gmail.com>
2017-01-13 17:12:39 +0700

    Fixed and Resolved with comments...

--- MathLib/Mathsblib.cs
+++ MathLib/Mathsblib.cs
@@ -4,41 +4,51 @@ namespace MathLib
 {
     public class Mathsblib
     {
+        //Method for Amicable Numebr use FOR LOOP;
         public static void Amicable(int min, int max)
         {
+            //Declare for variables;
             int sumA = 0, sumB = 0, temp = 0;
 
+            //Run for loop from min->max value;
             for(int i = min; i <= max; i++)
             {
-                sumA = 0;
-                sumB = 0;
+                sumA = 0; //reset sumA to zero;
+                sumB = 0; //reset sumB to zero;
 
+                //Run for loop from j to i-1;
                 for(int j = 1; j < i; j++)
                 {
-                    if (i % j == 0)
-                        sumA += j;
+                    if (i % j == 0) //Modulo i value with j value, if equal zero, sumA continue plus with j and store it for a round of i;
+                        sumA += j; //EX: j=1 --> sumA +=1 -> sumA = 1; j=2 --> sumA +=2 -> sumA = 3; [....];
                 }
 
+                //Run for loop from k to sumA-1;
                 for(int k = 1; k < sumA; k++)
                 {
-                    if (sumA % k == 0)
-                        sumB += k;
+                    if (sumA % k == 0) //Modulo sumA value with k value, if equal zero, sumB continue plus with k and store if for a round of i;
+                        sumB += k; //Like sumA +=j;
                 }
 
+                //Condition for i equal sumA, after if over a round loop (i), ex: i = 6 -> [1+2+3] ; sumA = 6 ; And then it i=sumA;
                 if (i == sumA)
-                    continue;
+                    continue; //To over a round of loop;
 
+                //Condition for i equal sumB, after it check in more condition, and temp equal i;
+                //it's mean that temp use for over a round, if the number of i equal to sumA;
+                //Ex: i=220;sumA=220; then it over one value of sumA, if it's equal; --> i = 220 | sumA = 284 ;
                 if (i == sumB)
                 {
-                    if (temp == i)
-                        continue;
+                    if (temp == i) //Check temp, if equal to i;
+                        continue; //And then over a round;
                     else
-                        temp = sumA;
-                        Console.WriteLine(i + " - " + sumB);
+                        temp = sumA; //Take value from sumA to temp, if condition is false; ex: sumA = 284 -> temp = 284; [....];
+                        Console.WriteLine(i + " - " + sumB); //Output the result to screen;
                 }
             }
         }
 
+        //Method for Amicable Numebr use WHILE LOOP;
         public static void Amicable2(int min, int max)
         {
             int sumA = 0, sumB = 0, temp = 0;
--- Pair_of_Amicable_Number/Program.cs
+++ Pair_of_Amicable_Number/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using MathLib;
+﻿using System; //Call system conponents;
+using MathLib; //Call namespace from Mathsblib.dll -> Its class : Mathsblib;
 
 namespace Pair_of_Amicable_Number
 {
@@ -7,7 +7,17 @@ namespace Pair_of_Amicable_Number
     {
         static void Main(string[] args)
         {
-            Mathsblib.Amicable2(1, 10000);
+            //Min = 1 ; Max = 10000;
+            //Run Method from Mathlib.dll (Class library)
+            Mathsblib.Amicable(1, 10000);
+
+            //You can call it as object;
+            /*
+             Mathsblib mathlib = new Mathsblib();
+             mathlib.Amicable(min,max);
+            */
+            //Mathsblib is the class that you add reference from Mathsblib.dll;
+            //mathlib is the name of the object that you was declared;
             Console.Read();
         }
     }
